f385131e2631675481887f0b2b01758164c265b6
edidk7 <d.ignacio292@gmail.com>
2020-10-15 19:53:56 -0300

    Fix Navbar (Closes #1)

--- js/main.js
+++ js/main.js
@@ -47,10 +47,13 @@ $(document).ready(function() {
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll < 50) {
+        console.log("muestra la wea");
         $('.fixed-top').css('background', 'transparent');
-        $('.fixed-top').css('transition', 'all 0.4s ease-in-out 0s');
+        $('.fixed-top').css('transition', 'all 0.1s ease-in-out 0s');
+        $('.fixed-top').css('border-bottom', '0px solid #fff');
     } else {
-        $('.fixed-top').css('background', '#ccc');
+        $('.fixed-top').css('background', '#182848');
+        $('.fixed-top').css('border-bottom', '1px solid #8393B4');
         $('.nav-link').css('background', '#');
         $('.fixed-top').css('transition', 'all 0.4s ease-in-out 0s');
 
